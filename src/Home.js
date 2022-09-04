fee7d4e7b44e5a2ce374c22e8dd9fbef3691065a
allisonkosborne <allisonkosborne@me.com>
2022-09-03 21:01:49 -0500

    css home page

--- src/Home.js
+++ src/Home.js
@@ -16,10 +16,21 @@ export const Home = () => {
         <div className="home-page">
           {/* <img src={"/image1.jpg"} alt="Flowers" /> */}
           <h1 className="welcome">MAPS</h1>
-          <h2 className="welcoming-saying">
-            Who have you seen [creeping,crawling,flying] around your
-            neighborhood?
-          </h2>
+          <div class="wrapper">
+            <h2 className="welcoming-saying">Who have you seen </h2>
+            <div class="words">
+              <span>creeping</span>
+              <span>flying</span>
+              <span>crawling</span>
+              <span>slithering</span>
+              <span>creeping</span>
+            </div>
+            <h2>around your neighborhood?</h2>
+          </div>
+          <button class="front-button" id="let-us-know">
+            Let Us Know!
+          </button>
+          <h5>***** Help us protect our local monsters! ****</h5>
         </div>
         <p>{/* <PropsAndState yourName={users.name} /> */}</p>
 
